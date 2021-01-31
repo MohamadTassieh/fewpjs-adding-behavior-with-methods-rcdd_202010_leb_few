1afcb604ac9ee98efe17c2add0dd5be4dee2ba85
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 13:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Cat {
     this.sex = sex;
   }
   speak() {
-   return Cat.name + " says meow!";
+   return this.name + " says meow!";
  }
 }
 
@@ -15,7 +15,7 @@ class Dog {
     this.sex = sex;
   }
   speak() {
-   return Dog.name + " says woof!";
+   return this.name + " says woof!";
  }
 }
 
